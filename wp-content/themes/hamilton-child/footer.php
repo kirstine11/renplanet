95da77d6581298de6d0e53485010ede9fb6a2e11
“valgfrit-men-brug-bare-dit-GitHub-brugernavn” <laer579a@stud.kea.dk>
2021-11-16 16:09:16 +0100

    footer

---
+++ wp-content/themes/hamilton-child/footer.php
@@ -0,0 +1,12 @@
+		</main>
+		
+		<footer class="site-footer section-inner">
+			
+<p>Copyright 2021</p>
+		</footer><!-- footer -->
+	    
+		
+	    <?php wp_footer(); ?>
+	        
+	</body>
+</html>
